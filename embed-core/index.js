5378fe594dc09f3a4f8d7e80490ff1f2397fc854
poluzhnikov <vitali@thebarkascollective.com>
2018-09-20 14:42:29 +0200

    volume control added via additional modules.js, new dependancy – loudness

--- embed-core/index.js
+++ embed-core/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();  
 const server = require('http').createServer(app);  
 const io = require('socket.io')(server);
+const loudness = require('loudness');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -13,7 +14,7 @@ io.on('connection', function(socket){
 
 	console.log('Socket connected');
 
-	axios.get('http://localhost/embed/embed-core/public/experiences.json')
+	axios.get('http://localhost:3000/experiences.json')
 		.then(response => {
 			socket.emit('update', response.data);
 		})
@@ -24,4 +25,20 @@ io.on('connection', function(socket){
 	socket.on('selected', (data)=> {
 		socket.broadcast.emit('load', data);
 	});
+	
+	
+  /*
+	  Volume
+	*/
+	
+	// Get & emit Current Volume
+	loudness.getVolume(function (err, volume_initial) {
+   	socket.broadcast.emit('volume-initial', volume_initial);
+	});
+
+	// Set new volume		
+  socket.on('volume-new', function(volume_new){  
+		loudness.setVolume(volume_new, function (err) {});  
+  });  	
+  
 });
---
+++ embed-core/public/controller-modules.js
@@ -0,0 +1,21 @@
+// Additional controller modules for later merge
+var socket = io();
+
+/*
+	Volume
+*/
+
+	// Set Initial Volume
+  socket.on('volume-initial', function(volume_initial){
+		$('#volume-slider').val(volume_initial).change();
+  });  
+	
+	// Set New Volume
+	$("#volume-slider").on("input change", function() {
+
+		var volume_new = $("#volume-slider").val();	
+	
+		socket.emit('volume-new', volume_new);
+
+	});
+		
