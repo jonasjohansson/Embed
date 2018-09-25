474e67f05e5daf303f8e2ccc69ebdad48687daad
Jonas Johansson <jns.johansson@gmail.com>
2018-09-25 15:03:15 +0200

    Controller example with HTML elements for Experience.json

--- embed-core/public/controller.js
+++ embed-core/public/controller.js
@@ -2,21 +2,31 @@ var socket = io();
 
 display = (experiences) => {
 	for (let prop in experiences){
-		let list = document.createElement('ul');
 		let experience = experiences[prop];
-		for (let prop in experience){
-			let item = experience[prop];
-			let listItem = document.createElement('li');
-			listItem.innerHTML = item;
-			list.appendChild(listItem);
-		}
-		let button = document.createElement('button');
-		button.innerHTML = 'command-play';
-		button.addEventListener('click', (event) => {
+
+		var $experience = document.createElement('div');
+
+		$experience.setAttribute('data-title', experience.title);
+		$experience.setAttribute('data-flags', experience.flags);
+		$experience.setAttribute('data-format', experience.format);
+
+		$experience.innerHTML = `
+			<video poster="${experience.cover_image}" style="height: 100px;">
+				<source src="${experience.cover_video}" type="video/mp4">
+			</video>
+			<h2>${experience.title}</h2>
+			<span class="description">${experience.description}</span>
+			<a href="${experience.author_url}"><span class="author">${experience.author}</span></a>`;
+
+		$button = document.createElement('button');
+		$button.innerHTML = 'command-play';
+
+		$button.addEventListener('click', (event) => {
 			socket.emit('play',experience);
 		})
-		list.appendChild(button);
-		document.querySelector('#experiences').appendChild(list);
+
+		$experience.appendChild($button);
+		document.querySelector('#experiences').appendChild($experience);
 	}
 }
 
@@ -26,4 +36,7 @@ play = (experience) => {
 		socket.emit('state-update',{state:'loading',val:false});
 		socket.emit('state-update',{state:'playing',val:true});
 	},3000);
+}
+
+createExperience = (experience) => {
 }
